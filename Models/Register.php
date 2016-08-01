c9078c9601ec3cb8059a7932951dad2924337fae
Sam <samuel.james.rothwell@gmail.com>
2016-08-01 14:21:12 +0100

    pdo works and can add users

--- Models/Register.php
+++ Models/Register.php
@@ -1,36 +1,44 @@
 <?php
 
-class Register {
+class Register
+{
 
 
+    public function addUser($email, $password, $address, $phone)
+    {
 
-
-    public function addUser($email,$password,$address,$phone) {
-        $servername = "helios.csesalford.com";
-        $username = "stb242";
-        $dbpassword = "jgw0Mqrj";
-        $dbname = "stb242";
+        //$servername = "helios.csesalford.com";
+        //$username = "stb242";
+        //$dbpassword = "jgw0Mqrj";
+        //$dbname = "stb242";
+        //&charset=utf8
 
         $emailtoadd = $email;
         $passwordtoadd = $password;
         $addresstoadd = $address;
         $phonetoadd = $phone;
 
+
+
         try {
-            $conn = new PDO("mysql:host=$servername;dbname=$dbname",$username,$dbpassword);
+            $db = new PDO('mysql:host=helios.csesalford.com;dbname=stb242', 'stb242', 'jgw0Mqrj');
+            echo("worked");
 
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $sql = "INSERT INTO users (email, password, address, phone) VALUES ($emailtoadd,$passwordtoadd,$addresstoadd,$phonetoadd)";
+            $query = $db->prepare('INSERT INTO users (email, password, address, phone) VALUES (:email, :password, :address, :phone) ');
 
-            $conn->exec($sql);
-            echo "Added successfully";
+            $query->bindParam(':email', $emailtoadd);
+            $query->bindParam(':password', $passwordtoadd);
+            $query->bindParam(':address', $addresstoadd);
+            $query->bindParam(':phone', $phonetoadd);
+            $query->execute();
 
+
+
+
+        } catch (PDOException $e) {
+            var_dump($e);
         }
-    catch(PDOException $e)
-    {
-        echo $sql . "<br>" . $e->getMessage();
-    }
-        $conn = null;
+
     }
 }
 ?>
--- register.php
+++ register.php
@@ -7,5 +7,7 @@ require_once('Models/Register.php');
 if (isset($_POST['Submit'])) {
     $register = new Register();
     $register->addUser($_POST['email'],$_POST['password'],$_POST['address'],$_POST['phone']);
+
+
 }
 require_once('Views/register.phtml');
